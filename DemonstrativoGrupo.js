789b2f69e0d265ae97d13c1cf32dedd27eff41ea
JefersonJunior <jeferson.j.dev@gmail.com>
2025-10-03 15:37:43 -0300

    Create DemonstrativoGrupo.js

---
+++ DemonstrativoGrupo.js
@@ -0,0 +1,172 @@
+[
+  {
+    "Discriminação": "Disponibilidades (Em: 31/07/2025) ....................",
+    "Saldo do Período": "3.652.822,58",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "Caixa..................................................................",
+    "Saldo do Período": "0",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "Depósitos Bancários.............................................",
+    "Saldo do Período": "3.274,29",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "Aplicações Financeiras..........................................",
+    "Saldo do Período": "0",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "Aplicações Financeiras do Grupo...........................",
+    "Saldo do Período": "801.690,25",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "Aplic. Fin. Vinculadas Contemplação......................",
+    "Saldo do Período": "2.847.858,04",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "Cheques em Cobrança..........................................",
+    "Saldo do Período": "0",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "(+) Recursos Coletados..................................",
+    "Saldo do Período": "2.002.205,08",
+    "Saldo Acumulado": "60.023.616,14"
+  },
+  {
+    "Discriminação": "Contribuições p/ Aq. de Bens................................",
+    "Saldo do Período": "1.686.005,98",
+    "Saldo Acumulado": "47.678.675,58"
+  },
+  {
+    "Discriminação": "Taxa de Administração..........................................",
+    "Saldo do Período": "232.936,80",
+    "Saldo Acumulado": "10.271.556,98"
+  },
+  {
+    "Discriminação": "Contribuições ao Fdo de Reserva...........................",
+    "Saldo do Período": "16.750,91",
+    "Saldo Acumulado": "505.853,26"
+  },
+  {
+    "Discriminação": "Rend. de Aplicações Financeiras............................",
+    "Saldo do Período": "36.001,13",
+    "Saldo Acumulado": "871.790,03"
+  },
+  {
+    "Discriminação": "Multas e Juros Moratórios....................................",
+    "Saldo do Período": "1.988,24",
+    "Saldo Acumulado": "41.850,05"
+  },
+  {
+    "Discriminação": "Prêmios de Seguros.............................................",
+    "Saldo do Período": "16.317,55",
+    "Saldo Acumulado": "282.724,40"
+  },
+  {
+    "Discriminação": "Custas Judiciais..............................................",
+    "Saldo do Período": "2,95",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "Reembolso de Despesas de Reg............................",
+    "Saldo do Período": "12.201,52",
+    "Saldo Acumulado": "355.638,96"
+  },
+  {
+    "Discriminação": "Outros.................................................................",
+    "Saldo do Período": "0",
+    "Saldo Acumulado": "15.294,94"
+  },
+  {
+    "Discriminação": "(-) Recursos Utilizados...................................",
+    "Saldo do Período": "1.941.969,96",
+    "Saldo Acumulado": "56.310.558,44"
+  },
+  {
+    "Discriminação": "Aquisição de Bens................................................",
+    "Saldo do Período": "1.677.391,57",
+    "Saldo Acumulado": "45.315.481,59"
+  },
+  {
+    "Discriminação": "Taxa de Administração...........................................",
+    "Saldo do Período": "232.965,46",
+    "Saldo Acumulado": "10.271.556,98"
+  },
+  {
+    "Discriminação": "Multas e Juros Moratórios.....................................",
+    "Saldo do Período": "1.005,50",
+    "Saldo Acumulado": "20.925,23"
+  },
+  {
+    "Discriminação": "Prêmios de Seguros.............................................",
+    "Saldo do Período": "18.041,95",
+    "Saldo Acumulado": "266.406,85"
+  },
+  {
+    "Discriminação": "Custas Judiciais...................................................",
+    "Saldo do Período": "0",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "Devolução a Cons. Desligados...............................",
+    "Saldo do Período": "0",
+    "Saldo Acumulado": "28.669,64"
+  },
+  {
+    "Discriminação": "Despesas de Reg. de Contrato..............................",
+    "Saldo do Período": "12.201,52",
+    "Saldo Acumulado": "355.638,96"
+  },
+  {
+    "Discriminação": "Rend. Pagos a Contemplados...............................",
+    "Saldo do Período": "0",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "Outros.................................................................",
+    "Saldo do Período": "363,96",
+    "Saldo Acumulado": "51.879,19"
+  },
+  {
+    "Discriminação": "Disponibilidades (Em: 31/08/2025) ..............",
+    "Saldo do Período": "3.713.057,70",
+    "Saldo Acumulado": "3.713.057,70"
+  },
+  {
+    "Discriminação": "Caixa..................................................................",
+    "Saldo do Período": "0",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "Depositos Bancários.............................................",
+    "Saldo do Período": "2.095,37",
+    "Saldo Acumulado": "2.095,37"
+  },
+  {
+    "Discriminação": "Aplicações Financeiras.........................................",
+    "Saldo do Período": "0",
+    "Saldo Acumulado": "0"
+  },
+  {
+    "Discriminação": "Aplicações Financeiras do Grupo........................",
+    "Saldo do Período": "776.284,97",
+    "Saldo Acumulado": "776.284,97"
+  },
+  {
+    "Discriminação": "Apl. Fin.Vinculadas Contemplação....................",
+    "Saldo do Período": "2.934.677,36",
+    "Saldo Acumulado": "2.934.677,36"
+  },
+  {
+    "Discriminação": "Cheques em Cobrança......................................",
+    "Saldo do Período": "0",
+    "Saldo Acumulado": "0"
+  }
+]
